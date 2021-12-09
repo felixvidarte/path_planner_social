cef43e5b66bafadfa4655261e59abcd93d358d4a
--- <--->
2021-12-09 10:08:26 +0100

    new

---
+++ path_planner_astar/src/CMakeFiles/path_planner_astar.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "../bin/path_planner_astar"
+  "../bin/path_planner_astar.pdb"
+  "CMakeFiles/path_planner_astar_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/path_planner_astar_autogen.dir/ParseCache.txt"
+  "path_planner_astar_autogen"
+  "CMakeFiles/path_planner_astar.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/collisions.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/genericmonitor.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/genericworker.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/grid.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/main.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/opt/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/opt/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/opt/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/path_planner_astar_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/specificmonitor.cpp.o"
+  "CMakeFiles/path_planner_astar.dir/specificworker.cpp.o"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "ui_mainUI.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path_planner_astar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ path_planner_astar/src/CMakeFiles/path_planner_astar_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path_planner_astar_autogen"
+  "path_planner_astar_autogen/include/ui_localUI.h"
+  "path_planner_astar_autogen/include/ui_mainUI.h"
+  "path_planner_astar_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/path_planner_astar_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
